64c14c6369c390e7945dc260babe837583e785bb
Marcin Wojdyr <wojdyr@gmail.com>
2013-02-01 19:44:31 +0000

    changes modified.py and c4/workflow.py

--- c4/workflow.py
+++ c4/workflow.py
@@ -1,16 +1,35 @@
 import os
 import sys
 from subprocess import Popen, PIPE
+import errno
 import threading
 import Queue
 import time
+import cPickle as pickle
+#import pickle
+import c4.mtz
+import c4.pdb
 
-from . import utils
+def put(text):
+    sys.stdout.write(text)
+
+def put_green(text):
+    if hasattr(sys.stdout, 'isatty') and sys.stdout.isatty():
+        put("\033[92m%s\033[0m" % text)
+    else:
+        put(text)
+
+def put_error(err, comment=""):
+    if hasattr(sys.stderr, 'isatty') and sys.stderr.isatty():
+        error_line = "Error: \033[91m%s\033[0m." % err # in bold red
+    else:
+        error_line = "Error: %s." % err
+    sys.stderr.write(error_line + "\n" + comment)
 
 
 class Job:
     def __init__(self, workflow, prog):
-        self.name = prog # name is only used to show info
+        self.name = os.path.basename(prog) or prog # only used to show info
         self.workflow = workflow
         self.args = [prog]
         self.std_input = ""
@@ -18,85 +37,102 @@ class Job:
         self.out = [] # either string or list of lines
         self.err = [] # either string or list of lines
         self.started = None # will be set to time.time() at start
-        self.elapsed = -1.0 # will be set when job ends
+        self.total_time = None # will be set when job ends
         self.parser = None
-        self.tmp = {} # parsing helpers
-        self.err_queue = None # parsing helpers
+        self.data = {} # parsing helpers
 
     def run(self):
-        self.workflow.run_job(job=self, show_progress=True)
-    def run_and_parse(self):
-        self.workflow.run_job(job=self, show_progress=True)
+        return self.workflow.run_job(job=self, show_progress=True)
 
+    def parse(self):
+        if self.parser:
+            p = getattr(self, self.parser)
+            return p()
+        else:
+            # generic non-parser shows output size if the job is finished
+            if self.total_time is None:
+                return ""
+            def size(out):
+                if not out:               return "-"
+                elif type(out) is list:   return "%dL" % len(out)
+                else:                     return "%.1fkB" % (len(out) / 1024.)
+            return "stdout:%7s stderr: %s" % (size(self.out), size(self.err))
 
-class Ccp4Job(Job):
-    def __init__(self, workflow, prog, logical=None, input="", add_end=True):
-        Job.__init__(self, workflow, prog)
-        if logical:
-            self.add_logical(logical)
-        if input or add_end:
-            self.add_input(input, add_end)
+    def _read_output(self):
+        while True:
+            try:
+                line = self.data['out_q'].get_nowait()
+            except Queue.Empty:
+                break
+            self.out.append(line)
+            yield line
 
-    def add_logical(self, loc):
-        "command line arguments that follow CCP4 logical-names convention"
-        for a in ["hklin", "hklout", "hklref", "xyzin", "xyzout"]:
-            if loc.get(a):
-                self.args.extend([a.upper(), loc[a]])
-
-    def add_input(self, lines, add_end):
-        "passing `keyworded' arguments through stdin is also CCP4 convention"
-        if isinstance(lines, basestring):
-            lines = lines.splitlines()
-        lines = [a.strip() for a in lines if a and not a.isspace()]
-        if add_end and not (lines and lines[-1].lower() == "end"):
-            lines.append("end")
-        if self.std_input:
-            self.std_input += "\n"
-        self.std_input += "\n".join(lines)
+    def _find_blobs_parser(self):
+        if "blobs" not in self.data:
+            sys.stdout.write("\n")
+            self.data["blobs"] = []
+        for line in self._read_output():
+            sys.stdout.write(line)
+            if line.startswith("#"):
+                sp = line.split()
+                score = float(sp[5])
+                if score > 150:
+                    xyz = tuple(float(x.strip(",()")) for x in sp[-3:])
+                    self.data["blobs"].append(xyz)
 
     def _refmac_parser(self):
-        t = self.tmp
+        t = self.data
         if "cycle" not in t:
             t["cycle"] = 0
             t["free_r"] = t["overall_r"] = 0.
-        while True:
-            try:
-                line = t['out_q'].get_nowait()
-                self.out.append(line)
-                if line.startswith("Free R factor"):
-                    t['free_r'] = float(line.split('=')[-1])
-                elif line.startswith("Overall R factor"):
-                    t['overall_r'] = float(line.split('=')[-1])
-                elif (line.startswith("     Rigid body cycle =") or
-                      line.startswith("     CGMAT cycle number =")):
-                    t['cycle'] = int(line.split('=')[-1])
-            except Queue.Empty:
-                break
-        return "#%(cycle)-2d  R-free / R = %(free_r).4f / %(overall_r).4f" % t
+        for line in self._read_output():
+            if line.startswith("Free R factor"):
+                t['free_r'] = float(line.split('=')[-1])
+            elif line.startswith("Overall R factor"):
+                t['overall_r'] = float(line.split('=')[-1])
+            elif (line.startswith("     Rigid body cycle =") or
+                  line.startswith("     CGMAT cycle number =")):
+                t['cycle'] = int(line.split('=')[-1])
+        return "cycle %(cycle)2d/%(ncyc)d   "\
+                "R-free / R = %(free_r).4f / %(overall_r).4f" % t
 
 
-def out_size(out):
-    if not out:
-        return "-"
-    elif type(out) == list:
-        return "%dL" % len(out)
-    else:
-        return "%.1fkB" % (len(out) / 1024.)
+def ccp4_job(workflow, prog, logical=None, input="", add_end=True):
+    """Handle traditional convention for arguments of CCP4 programs.
+    logical is dictionary with where keys are so-called logical names,
+    input string or list of lines that are to be passed though stdin
+    add_end adds "end" as the last line of stdin
+    """
+    job = Job(workflow, prog)
+    if logical:
+        for a in ["hklin", "hklout", "hklref", "xyzin", "xyzout"]:
+            if logical.get(a):
+                job.args.extend([a.upper(), logical[a]])
+    lines = (input.splitlines() if isinstance(input, basestring) else input)
+    stripped = [a.strip() for a in lines if a and not a.isspace()]
+    if add_end and not (stripped and stripped[-1].lower() == "end"):
+        stripped.append("end")
+    if job.std_input:
+        job.std_input += "\n"
+    job.std_input += "\n".join(lines)
+    return job
 
-def log(text):
-    sys.stdout.write(text)
+
+_jobindex_fmt = "%3d "
+_jobname_fmt = "%-15s"
+_elapsed_fmt = "%5.1fs  "
 
 def _print_elapsed(job, event):
     while not event.wait(0.5):
-        text = "elapsed:%5.1fs" % (time.time() - job.started)
-        if job.parser:
-            text += "  " + job.parser()
-        log(text)
-        sys.stdout.flush()
-        log("\b"*len(text))
+        p = job.parse()
+        if p is not None:
+            text = (_elapsed_fmt % (time.time() - job.started)) + p
+            put(text)
+            sys.stdout.flush()
+            put("\b"*len(text))
 
 
-def start_enqueue_thread(file_obj):
+def _start_enqueue_thread(file_obj):
     def enqueue_lines(f, q):
         for line in iter(f.readline, b''):
             q.put(line)
@@ -107,91 +143,139 @@ def start_enqueue_thread(file_obj):
     thr.start()
     return thr, que
 
+def _write_output(output, filename):
+    with open(filename, "w") as f:
+        if type(output) is list:
+            for line in output:
+                f.write(line)
+        else:
+            f.write(output)
+
+
 class Workflow:
     def __init__(self, output_dir):
         self.output_dir = os.path.abspath(output_dir)
-        self.job_log = []
+        self.jobs = []
         if not os.path.isdir(self.output_dir):
             os.mkdir(self.output_dir)
 
-    def run_job(self, job, show_progress, dry_run=False):
+    def pickle_jobs(self, filename="jobs.pickle"):
         os.chdir(self.output_dir)
-        self.job_log.append(job)
-        log("%3d  %-30s" % (len(self.job_log), utils.green(job.name)))
+        with open(filename, "wb") as f:
+            pickle.dump(self.jobs, f, -1)
+
+    def run_job(self, job, show_progress):
+        if not hasattr(sys.stdout, 'isatty') or not sys.stdout.isatty():
+            show_progress = False
+        os.chdir(self.output_dir)
+        self.jobs.append(job)
+        put(_jobindex_fmt % len(self.jobs))
+        put_green(_jobname_fmt % job.name)
         sys.stdout.flush()
         job.started = time.time()
-        if dry_run:
-            log("--- DRY RUN\n%s\n%s\n---\n" % (job.args, job.std_input))
+        final_mesg = None
+        #job.args[0] = "true" # for debugging
+        p = Popen(job.args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+        if show_progress:
+            event = threading.Event()
+            progress_thread = threading.Thread(target=_print_elapsed,
+                                               args=(job, event))
+            progress_thread.daemon = True
+            progress_thread.start()
+
+        if job.parser:
+            out_t, job.data['out_q'] = _start_enqueue_thread(p.stdout)
+            err_t, job.data['err_q'] = _start_enqueue_thread(p.stderr)
+            try:
+                p.stdin.write(job.std_input)
+            except IOError as e:
+                put("\nWarning: passing std input to %s failed.\n" % job.name)
+                if e.errno not in (errno.EPIPE, e.errno != errno.EINVAL):
+                    raise
+            p.stdin.close()
+            out_t.join()
+            err_t.join()
+            p.wait()
+            # nothing is written to the queues at this point
+            # parse what's left in the queues
+            final_mesg = job.parse()
+            # take care of what is left by the parser
+            while not job.data['out_q'].empty():
+                job.out.append(job.data['out_q'].get_nowait())
+            while not job.data['err_q'].empty():
+                job.err.append(job.data['err_q'].get_nowait())
+            job.data['out_q'] = None
+            job.data['err_q'] = None
         else:
-            p = Popen(job.args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-            if show_progress:
-                event = threading.Event()
-                progress_thread = threading.Thread(target=_print_elapsed,
-                                                   args=(job, event))
-                progress_thread.daemon = True
-                progress_thread.start()
-                if job.parser:
-                    out_t, job.tmp['out_q'] = start_enqueue_thread(p.stdout)
-                    err_t, job.tmp['err_q'] = start_enqueue_thread(p.stderr)
-                    p.stdin.write(job.std_input)
-                    p.stdin.close()
-                    out_t.join()
-                    err_t.join()
-                    p.wait()
-                    # nothing is written to the queues at this point
-                    while not job.tmp['out_q'].empty():
-                        job.out.append(job.tmp['out_q'].get_nowait())
-                    while not job.tmp['err_q'].empty():
-                        job.err.append(job.tmp['err_q'].get_nowait())
-                else:
-                    job.out, job.err = p.communicate(input=job.std_input)
-                event.set()
-                progress_thread.join()
-            else:
-                job.out, job.err = p.communicate(input=job.std_input)
-            job.elapsed = time.time() - job.started
-            retcode = p.poll()
-            if retcode:
-                raise RuntimeError("Program failed: %s" % job.args)
-        log("elapsed:%5.1fs  " % job.elapsed)
-        if not job.parser:
-            log("stdout:%7s stderr: %s" % (out_size(job.out),
-                                           out_size(job.err)))
-        log("\n")
+            job.out, job.err = p.communicate(input=job.std_input)
+            final_mesg = job.parse()
+
+        if show_progress:
+            event.set()
+            progress_thread.join()
+
+        job.total_time = time.time() - job.started
+        retcode = p.poll()
+        if retcode:
+            raise RuntimeError("Program failed: %s" % job.args)
+        put(_elapsed_fmt % job.total_time)
+        put((final_mesg or "")+ "\n")
+
+        log_basename = "%02d-%s" % (len(self.jobs), job.name.replace(" ","_"))
+        if job.out:
+            _write_output(job.out, "%s.log" % log_basename)
+            job.out = "-> %s.log" % log_basename
+        if job.err:
+            _write_output(job.err, "%s.err" % log_basename)
+            job.err = "-> %s.err" % log_basename
+
+        return job
 
     def change_pdb_cell(self, xyzin, xyzout, cell):
         #for now using pdbset
         self.pdbset(xyzin=xyzin, xyzout=xyzout, cell=cell).run()
 
+    def read_pdb_metadata(self, xyzin):
+        return c4.pdb.read_metadata(xyzin)
+
+    def read_mtz_metadata(self, hklin):
+        return c4.mtz.read_metadata(hklin)
+
+    def molrep(self, f, m):
+        job = Job("molrep")
+        job.args.extend(["-f", f, "-m", m])
+        return job
+
     def pointless(self, hklin, xyzin, hklref=None, hklout=None, keys=""):
-        return Ccp4Job(self, "pointless", logical=locals(), input=keys)
+        return ccp4_job(self, "pointless", logical=locals(), input=keys)
 
     def mtzdump(self, hklin, keys=""):
-        return Ccp4Job(self, "mtzdump", logical=locals())
+        return ccp4_job(self, "mtzdump", logical=locals())
 
     def unique(self, hklout, cell, symmetry, resolution,
                labout="F=F_UNIQUE SIGF=SIGF_UNIQUE"):
-        return Ccp4Job(self, "unique", logical=locals(),
-                       input=["cell %g %g %g %g %g %g" % cell,
-                              "symmetry '%s'" % symmetry,
-                              "resolution %.3f" % resolution,
-                              "labout %s" % labout])
+        return ccp4_job(self, "unique", logical=locals(),
+                        input=["cell %g %g %g %g %g %g" % cell,
+                               "symmetry '%s'" % symmetry,
+                               "resolution %.3f" % resolution,
+                               "labout %s" % labout])
 
     def freerflag(self, hklin, hklout):
-        return Ccp4Job(self, "freerflag", logical=locals())
+        return ccp4_job(self, "freerflag", logical=locals())
 
     def reindex(self, hklin, hklout, symmetry):
-        return Ccp4Job(self, "reindex", logical=locals(),
-                       input=["symmetry '%s'" % symmetry,
-                              "reindex h,k,l"])
+        return ccp4_job(self, "reindex", logical=locals(),
+                        input=["symmetry '%s'" % symmetry,
+                               "reindex h,k,l"])
 
     def truncate(self, hklin, hklout, labin, labout):
-        return Ccp4Job(self, "truncate", logical=locals(),
-                       input=["labin %s" % labin, "labout %s" % labout])
+        return ccp4_job(self, "truncate", logical=locals(),
+                        input=["labin %s" % labin, "labout %s" % labout])
 
     def cad(self, hklin, hklout, keys):
         assert type(hklin) is list
-        job = Ccp4Job(self, "cad", logical={}, input=keys)
+        job = ccp4_job(self, "cad", logical={}, input=keys)
         # is hklinX only for cad?
         for n, name in enumerate(hklin):
             job.args += ["HKLIN%d" % (n+1), name]
@@ -199,19 +283,27 @@ class Workflow:
         return job
 
     def pdbset(self, xyzin, xyzout, cell):
-        return Ccp4Job(self, "pdbset", logical=locals(),
-                       input=["cell %g %g %g %g %g %g" % cell])
+        return ccp4_job(self, "pdbset", logical=locals(),
+                        input=["cell %g %g %g %g %g %g" % cell])
 
     def refmac5(self, hklin, xyzin, hklout, xyzout, labin, labout, keys):
-        job = Ccp4Job(self, "refmac5", logical=locals(),
-                      input=(["labin %s" % labin, "labout %s" % labout] +
+        job = ccp4_job(self, "refmac5", logical=locals(),
+                       input=(["labin %s" % labin, "labout %s" % labout] +
                               keys.splitlines()))
         words = keys.split()
-        for n, w in enumerate(words[:-1]):
+        for n, w in enumerate(words[:-2]):
             if w == "refinement" and words[n+1] == "type":
                 job.name += " " + words[n+2][:5]
-            elif w.startswith("ncyc"):
-                job.name += "*" + words[n+1]
-        job.parser = job._refmac_parser
+        job.data['ncyc'] = -1
+        for n, w in enumerate(words[:-1]):
+            if w.startswith("ncyc"):
+                job.data['ncyc'] = int(words[n+1])
+        job.parser = "_refmac_parser"
+        return job
+
+    def find_blobs(self, mtz, pdb, sigma=1.0):
+        job = Job(self, os.path.join(os.path.dirname(__file__), "find-blobs"))
+        job.args += ["-s%g" % sigma, mtz, pdb]
+        job.parser = "_find_blobs_parser"
         return job
 
--- modified.py
+++ modified.py
@@ -6,53 +6,64 @@ Usage: dimple input.mtz input.pdb output_dir
 
 import os
 import sys
-import c4.workflow
-import c4.mtz
-import c4.pdb
-import c4.utils
+from c4.workflow import Workflow, put, put_error
 
 def run_pipeline(wf, pdb, mtz):
+    put("Change mtz symmetry if needed. Use pdb as reference.\n")
     wf.pointless(hklin=mtz, xyzin=pdb, hklout="pointless.mtz").run()
-    pdb_meta = c4.pdb.read_metadata(pdb)
-    mtz_meta = c4.mtz.read_metadata(mtz)
+    pdb_meta = wf.read_pdb_metadata(pdb)
+    mtz_meta = wf.read_mtz_metadata(mtz)
+    if mtz_meta.symmetry == pdb_meta.symmetry:
+        put(" Same symmetry in pdb and mtz (%s).\n" % pdb_meta.symmetry)
+        truncate_hklin = "pointless.mtz"
+    else:
+        put(" Different symmetry in pdb (%s) and mtz (%s), reindexing mtz\n" %
+                (pdb_meta.symmetry, mtz_meta.symmetry))
+        wf.reindex(hklin="pointless.mtz", hklout="spacegroup.mtz",
+                   symmetry=pdb_meta.symmetry).run()
+        truncate_hklin = "spacegroup.mtz"
+    put("Obtain structure factor amplitudes\n")
+    wf.truncate(hklin=truncate_hklin, hklout="truncate.mtz",
+                labin="IMEAN=IMEAN SIGIMEAN=SIGIMEAN",
+                labout="F=F SIGF=SIGF").run()
+    put("Add missing reflections and flag for cross-validation\n")
     wf.unique(hklout="unique.mtz",
               cell=mtz_meta.cell, symmetry=pdb_meta.symmetry,
               resolution=mtz_meta.resolution_range[1],
               labout="F=F_UNIQUE SIGF=SIGF_UNIQUE").run()
     wf.freerflag(hklin="unique.mtz", hklout="free.mtz").run()
-    if mtz_meta.symmetry == pdb_meta.symmetry:
-        print "Same symmetry in pdb and mtz (%s), good." % pdb_meta.symmetry
-        os.symlink("pointless.mtz", "spacegroup.mtz")
-    else:
-        print "Different symmetry in pdb (%s) and mtz (%s), reindexing mtz" % (
-                pdb_meta.symmetry, mtz_meta.symmetry)
-        wf.reindex(hklin="pointless.mtz", hklout="spacegroup.mtz",
-                   symmetry=pdb_meta.symmetry).run()
-    wf.truncate(hklin="spacegroup.mtz", hklout="truncate.mtz",
-                labin="IMEAN=IMEAN SIGIMEAN=SIGIMEAN",
-                labout="F=F SIGF=SIGF").run()
     wf.cad(hklin=["truncate.mtz", "free.mtz"], hklout="prepared.mtz",
            keys="""labin file 1 ALL
                    labin file 2 ALL""").run()
     if all(pdb_meta.cell[i] - mtz_meta.cell[i] < 1e-3 for i in range(6)):
-        print "Cell dimensions in pdb and mtz are the same, good."
-        os.symlink(pdb, "prepared.pdb")
+        put("Cell dimensions in pdb and mtz are the same.\n")
+        refmac_rigid_xyzin = pdb
     else:
-        print "Different cell in pdb %s ..." % str(pdb_meta.cell)
-        print "              and mtz %s, changing pdb" % str(mtz_meta.cell)
+        put("Different cell in pdb %s ...\n" % str(pdb_meta.cell))
+        put("              and mtz %s, changing pdb\n" % str(mtz_meta.cell))
         wf.change_pdb_cell(xyzin=pdb, xyzout="prepared.pdb",
                            cell=mtz_meta.cell)
+        refmac_rigid_xyzin = "prepared.pdb"
     refmac_labin = "FP=F SIGFP=SIGF FREE=FreeR_flag"
     refmac_labout = ("FC=FC PHIC=PHIC FWT=2FOFCWT PHWT=PH2FOFCWT "
                      "DELFWT=FOFCWT PHDELWT=PHFOFCWT")
-    wf.refmac5(hklin="prepared.mtz", xyzin="prepared.pdb",
+    put("Rigid-body refinement.\n")
+    wf.refmac5(hklin="prepared.mtz", xyzin=refmac_rigid_xyzin,
                hklout="refmacRB.mtz", xyzout="refmacRB.pdb",
                labin=refmac_labin, labout=refmac_labout,
                keys="""refinement type rigidbody resolution 15 3.5
                        scale type simple lssc anisotropic experimental
                        solvent yes vdwprob 1.4 ionprob 0.8 mshrink 0.8
-                       rigidbody ncycle 10""").run_and_parse()
-    wf.refmac5(hklin="prepared.mtz", xyzin="refmacRB.pdb",
+                       rigidbody ncycle 10""").run()
+    if wf.jobs[-1].data["free_r"] > 0.4:
+        put("Run MR for R_free > 0.4\n")
+        wf.molrep(f="prepared.mtz", m="refmacRB.pdb")
+        refmac_xyzin="molrep.pdb"
+    else:
+        put("No MR for R_free < 0.4\n")
+        refmac_xyzin="refmacRB.pdb"
+    put("Finally, restrained refinement.\n")
+    wf.refmac5(hklin="prepared.mtz", xyzin=refmac_xyzin,
                hklout="final.mtz", xyzout="final.pdb",
                labin=refmac_labin, labout=refmac_labout,
                keys="""make hydrogen all hout no cispeptide yes ssbridge yes
@@ -60,7 +71,8 @@ def run_pipeline(wf, pdb, mtz):
                        weight matrix 0.2
                        scale type simple lssc anisotropic experimental
                        solvent yes vdwprob 1.4 ionprob 0.8 mshrink 0.8
-                       ncycle 8""").run_and_parse()
+                       ncycle 8""").run()
+    wf.find_blobs("final.mtz", "final.pdb", sigma=0.8).run()
 
 
 def main():
@@ -79,10 +91,11 @@ def main():
         for filename in [mtz, pdb]:
             assert os.path.isfile(filename), "File not found: " + filename
     except AssertionError, e:
-        sys.stderr.write(c4.utils.red("Error: %s." % e) + "\n%s" % __doc__)
+        put_error(e, __doc__)
         sys.exit(1)
-    wf = c4.workflow.Workflow(output_dir)
+    wf = Workflow(output_dir)
     run_pipeline(wf=wf, mtz=os.path.abspath(mtz), pdb=os.path.abspath(pdb))
+    wf.pickle_jobs()
 
 if __name__ == "__main__":
     main()
