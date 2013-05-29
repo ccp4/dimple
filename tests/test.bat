3d1c933955835eb990d92d5b0b6164a14e874b2c
Marcin Wojdyr <wojdyr@gmail.com>
2013-05-29 12:11:25 +0100

    added test.bat

---
+++ tests/test.bat
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+
+@setlocal
+@set testdir=%~dp0
+
+@echo "Run dimple on artificial thaumatin example"
+@set PYTHONPATH=%testdir%..;%PYTHONPATH%
+@ccp4.python %testdir%..\dimple.py %testdir%\thaumatin.mtz %testdir%\thaumatin.pdb %testdir%\out %*
