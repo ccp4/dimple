a6d750c98318b8d336122a1148bb0c0a33a54e6a
Marcin Wojdyr <wojdyr@gmail.com>
2013-05-29 14:59:47 +0100

    add white-space quoting to test.bat

--- tests/test.bat
+++ tests/test.bat
@@ -1,7 +1,7 @@
 @setlocal
 @set testdir=%~dp0
 
-@echo "Run dimple on artificial thaumatin example"
-@set PYTHONPATH=%testdir%..;%PYTHONPATH%
-@ccp4.python %testdir%..\dimple.py %testdir%\thaumatin.mtz %testdir%\thaumatin.pdb %testdir%\out %*
+@echo      === Run dimple on artificial thaumatin example ===
+@set PYTHONPATH=%testdir%..
+@call ccp4.python "%testdir%..\dimple.py" "%testdir%thaumatin.mtz" "%testdir%thaumatin.pdb" "%testdir%out" %*
 pause
