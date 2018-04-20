26f9adb4c028294f3c97217189a01fe5058d844a
Marcin Wojdyr <wojdyr@gmail.com>
2018-04-20 13:04:47 +0100

    fix tests/test.bat

--- tests/test.bat
+++ tests/test.bat
@@ -3,5 +3,5 @@
 
 @echo      === Run dimple on artificial thaumatin example ===
 @set PYTHONPATH=%testdir%..
-@call ccp4-python "%testdir%..\dimple.py" "%testdir%thaumatin.mtz" "%testdir%thaumatin.pdb" "%testdir%out" %*
+@call ccp4-python "%testdir%..\main.py" "%testdir%thaumatin.mtz" "%testdir%thaumatin.pdb" "%testdir%out" %*
 @pause
