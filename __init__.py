f895a7d3459352681cebe538430e43f789178967
Marcin Wojdyr <wojdyr@gmail.com>
2025-12-04 17:29:13 +0100

    drop Py2.7 compatibility

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 
 import sys
-if sys.version_info[:2] != (2, 7) and sys.version_info[:2] < (3, 5):
-    sys.stderr.write('Error. Python 2.7 or 3.5+ is required.\n')
+if sys.version_info[:2] < (3, 5):
+    sys.stderr.write('Error. Python 3.5+ is required.\n')
     sys.exit(1)
